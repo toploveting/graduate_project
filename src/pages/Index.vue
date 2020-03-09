943f5ef3beb6204593414d2cd271a0f89b3b8d6a
tingting <tingtinglin159@gmail.com>
2020-03-09 13:47:01 +0800

    one

---
+++ src/pages/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="q-pa-md q-gutter-sm">
+      <q-btn unelevated to="/pages/list" size="25px" round color="deep-orange" >
+      <q-icon name="access_time" style= "color:#fff ; font-size: 2.3em;" />
+      </q-btn>
+      <br>
+      <q-btn unelevated to="/pages/form" size="25px" round color="deep-orange" >
+      <q-icon name="calendar_today" style= "color:#fff ; font-size: 2em;" />
+      </q-btn>
+    </div>
+    <img
+      alt="bg"
+      src="~assets/background.png"
+    />
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
